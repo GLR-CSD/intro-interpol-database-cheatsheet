749cb3be89c1c8dae19daff39752dec86f553121
Jeffrey Arts <arts@glr.nl>
2024-09-10 13:30:29 +0200

    Add dist directory to Repo for plesk

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-import { ViteSSG } from "vite-ssg"
 import App from "./App.vue"
-import { routerOptions } from "@/routes"
+import { createApp } from "vue"
+import router from "@/routes"
 import { createPinia } from "pinia"
 import { VueHeadMixin } from "@unhead/vue"
 // Locale i18n packages
@@ -13,32 +13,24 @@ import $text from "@/services/vue-i18n-markdown"
 import "./assets/scss/index.scss"
 
   
-export const createApp = ViteSSG(
-    App,
-    routerOptions ,
-    // function to have custom setups
-    ({ app, router, routes, isClient, initialState }) => {
-        
-        const pinia = createPinia()
+const pinia = createPinia()
 
-        const i18n = createI18n({
-            legacy: false,
-            warnHtmlMessage: import.meta.env.DEV,
-            fallbackLocale: "en",
-            messages,
-        })
-        
-        
-        app.use(i18n)
-        app.use(pinia)
-        app.mixin(VueHeadMixin)
+pinia.use(({ store }) => {
+    if (store.$id === "locale") {
+        store.$i18n = i18n.global
+    }
+})
 
-        app.config.globalProperties.$text  = $text
-        
-        pinia.use(({ store }) => {
-            if (store.$id === "locale") {
-                store.$i18n = i18n.global
-            }
-        })
-    },
-)
+const i18n = createI18n({
+    legacy: false,
+    warnHtmlMessage: import.meta.env.DEV,
+    fallbackLocale: "en",
+    messages,
+})
+
+
+createApp(App)
+    .use(pinia)
+    .use(i18n)
+    .use(router)
+    .mount("#app")
